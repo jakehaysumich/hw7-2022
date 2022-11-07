744fbdb203fba94a59b01fc4c7685dfc317afe10
jakehaysumich <112513191+jakehaysumich@users.noreply.github.com>
2022-11-07 10:55:56 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,66 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	video = document.querySelector('#player1')
+	video.autoplay = false
+	video.loop = false
+	video.muted = false
+	console.log(video)
+
+
+});
+document.querySelector("#play").addEventListener("click", function(){
+	console.log("play video")
+	document.querySelector("#volume").innerHTML = video.volume * 100 + '%'
+	video.play()
+})
+document.querySelector("#pause").addEventListener("click", function(){
+	console.log("pause video")
+	video.pause()})
+
+
+document.querySelector("#slower").addEventListener("click", function(){
+	video.playbackRate = video.playbackRate * .9 
+	console.log("slowed down to "+video.playbackRate)
+})
+
+document.querySelector("#faster").addEventListener("click", function(){
+	video.playbackRate = video.playbackRate + 1.1*video.playbackRate*.1
+	console.log("sped up to "+video.playbackRate)
+})
+
+document.querySelector("#skip").addEventListener("click", function(){
+	video.currentTime += 10 
+	console.log("Jumped 10 to "+ video.currentTime)
+	if(video.ended == true){video.currentTime = 0;
+	console.log("Jumped past duration restart to "+video.currentTime)}
+})
+
+document.querySelector("#mute").addEventListener("click", function(){
+	if(video.muted == true){
+		video.muted = false
+		console.log("volume unmuted set to "+video.volume)
+		mute.textContent = "Mute"}
+	else{ 
+		video.muted = true
+		console.log("volume muted")
+		mute.textContent = 'Unmute';}
+})
+
+document.querySelector("#slider").addEventListener("click", function(){
+	video.volume = range.value
+	console.log("volume set to "+video.volume)})
+
+document.querySelector("#vintage").addEventListener("click", function(){
+	video.classList.add("oldSchool")
+	console.log("old school styled")})
+
+document.querySelector("#orig").addEventListener("click", function(){
+	video.classList.remove("oldSchool")
+	console.log("original styling")})
+	
+
+// document.querySelector("#play").addEventListener("click", function() {
+// 	console.log("Play Video");
+// });
